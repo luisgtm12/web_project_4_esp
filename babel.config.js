cbc2706000a1cbe52921965b8f442c71a215c7bf
luisgtm12 <120407727+luisgtm12@users.noreply.github.com>
2023-09-21 11:41:23 -0500

    Implementacion de webpack

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+const presets = [
+  ['@babel/preset-env', { // preset que quieres utilizar
+    targets: { // versiones de navegadores en las que queremos que nuestro código sea compatible
+      edge: '17',
+      ie: '11',
+      firefox: '50',
+      chrome: '64',
+      safari: '11.1'
+    },
+
+    useBuiltIns: "entry",
+    corejs: '^3',
+  }]
+];
+
+module.exports = { presets };
